6dbb87ad30318000f05ae79ffeaa64ca2f02afaa
Magicked <hausrath@gmail.com>
2017-01-30 16:20:36 -0500

    Added an example local_settings.py

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 Goofy little django app to track foosball games. Ranks players via the Elo rating.
 
 ## Installation
+Copy tabletracker/example_local_settings.py to tabletracker/local_settings.py.
+
 In tabletracker/local_settings.py, generate your SECRET_KEY. Install as a normal Django app.
---
+++ tabletracker/example_local_settings.py
@@ -0,0 +1,18 @@
+# These are local settings that are ignored by git
+LOCAL_SETTINGS = True
+from tabletracker.settings import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+# from django.utils.crypto import get_random_string as grs
+# print grs(50, 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)')
+SECRET_KEY = ''
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
